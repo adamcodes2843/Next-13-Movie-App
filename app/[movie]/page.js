0711f46200e3b0f0dd18546db3d43c8d57d79736
Adam Childers <adamcodes2843@gmail.com>
2023-02-20 14:43:08 -0600

    Completed a next 13 crash course and  movie app. I might add other pages and features.

---
+++ app/[movie]/page.js
@@ -0,0 +1,28 @@
+import Image from "next/image"
+
+export async function generateStaticParams(){
+    const data= await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+    const res = await data.json()
+    return res.results.map((movie) => ({
+        movie: toString(movie.id)
+    }))
+}
+
+export default async function MovieDetail({params}) {
+    const { movie } = params
+    const imagePath = "https://image.tmdb.org/t/p/original"
+    const data= await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`)
+    const res = await data.json()
+    return (
+        <div>
+            <div>
+                <h2 className="text-2xl">{res.title}</h2>
+                <h2>{res.release_date}</h2>
+                <h2>Runtime: {res.runtime} minutes</h2>
+                <h2 className="text-sm bg-green-600 inline-block my-2 py-2 px-4 rounded">{res.status}</h2>
+                <Image className="my-4 w-full" src={imagePath + res.backdrop_path} width={1000} height={1000} alt={res.title} priority/>
+                <p className="px-4">{res.overview}</p>
+            </div>
+        </div>
+    )
+}
